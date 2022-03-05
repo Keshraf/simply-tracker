b4b73bda51cf4c37d8a06830d4d2a81218837984
Ketan Saraf <saraf.ketan1108@gmail.com>
2022-03-06 00:29:14 +0530

    📃Added Pages

---
+++ src/pages/Habits/Habits.js
@@ -0,0 +1,9 @@
+const Habits = () => {
+  return (
+    <>
+      <h1>Habits Page</h1>
+    </>
+  );
+};
+
+export default Habits;
---
+++ src/pages/Home/Home.js
@@ -0,0 +1,23 @@
+const Home = () => {
+  const info = [
+    {
+      habit: "Run Everyday",
+      goal: 8,
+      done: 3,
+    },
+    {
+      habit: "Drink Water",
+      goal: 10,
+      done: 5,
+    },
+  ];
+
+  return (
+    <div>
+      <h1>Home Page</h1>
+      <p>uwr9gfv7whru9fwhufhew9vhuwh9vs</p>
+    </div>
+  );
+};
+
+export default Home;
