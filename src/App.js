e4fac10f1ddf23e8435948775ab0a4bb7d5a6b3b
Ketan Saraf <saraf.ketan1108@gmail.com>
2022-03-06 00:30:19 +0530

    🎨 Designed and Added Assets

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
-import logo from "./logo.svg";
 import "./App.css";
+import Home from "./pages/Home/Home";
+import Create from "./pages/Create/Create";
+import Habits from "./pages/Habits/Habits";
+import Analytics from "./pages/Analytics/Analytics";
+import Navigation from "./components/Navigation/Navigation";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello World!!</h1>
-    </div>
+    <main>
+      <BrowserRouter>
+        <Navigation />
+        <section className="App">
+          <Routes>
+            <Route path="/" element={<Home />} exact />
+            <Route path="/create" element={<Create />} />
+            <Route path="/analytics" element={<Analytics />} />
+            <Route path="/habits" element={<Habits />} />
+          </Routes>
+        </section>
+      </BrowserRouter>
+    </main>
   );
 }
 
